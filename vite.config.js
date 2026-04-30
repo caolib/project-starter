088290e51788106e0c17ff457f8ef4217f2c0387
caolib <caolibx@qq.com>
2026-04-30 21:00:41 +0800

    🐳 chore: 迁移到 vite 8

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import alias from '@rollup/plugin-alias'
 import VueDevTools from 'vite-plugin-vue-devtools'
-import path from 'path'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    VueDevTools(),
-    alias({
-      entries: [
-        { find: '@', replacement: path.resolve(__dirname, 'src') }
-      ]
-    })
+    VueDevTools()
   ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   base: './'
 })
